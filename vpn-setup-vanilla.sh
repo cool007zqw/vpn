1ad145c91115a5ae68e83003f5678efc0df4d2f1
cool007zqw <cool007zqw@163.com>
2014-06-19 23:00:31 +0800

    Update vpn-setup-vanilla.sh

--- vpn-setup-vanilla.sh
+++ vpn-setup-vanilla.sh
@@ -24,18 +24,20 @@ PUB_IP="27.255.66.50"
 rpm -i http://poptop.sourceforge.net/yum/stable/rhel6/pptp-release-current.noarch.rpm
 yum -y install ppp pptpd
 
-echo "1" > /proc/sys/net/ipv4/ip_forward
-sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
-
 
 echo "localip $VPN_LOCAL" >> /etc/pptpd.conf # Local IP address of your VPN server
 echo "remoteip $VPN_REMOTE" >> /etc/pptpd.conf # Scope for your home network
+
+echo "$VPN_USER pptpd $VPN_PASS *" >> /etc/ppp/chap-secrets
+
+echo "1" > /proc/sys/net/ipv4/ip_forward
 
 echo "ms-dns 8.8.8.8" >> /etc/ppp/options.pptpd # Google DNS Primary
 echo "ms-dns 209.244.0.3" >> /etc/ppp/options.pptpd # Level3 Primary
 echo "ms-dns 208.67.222.222" >> /etc/ppp/options.pptpd # OpenDNS Primary
 
-echo "$VPN_USER pptpd $VPN_PASS *" >> /etc/ppp/chap-secrets
+sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+
 sysctl -p
 
 iptables -A INPUT -i venet0 -p tcp --dport 1723 -j ACCEPT
