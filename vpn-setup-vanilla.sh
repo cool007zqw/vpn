b968ee067cc9db8fdb1c1e2e92eb672be7709ef3
cool007zqw <cool007zqw@163.com>
2014-06-19 22:48:25 +0800

    Update vpn-setup-vanilla.sh

--- vpn-setup-vanilla.sh
+++ vpn-setup-vanilla.sh
@@ -8,28 +8,25 @@
 # @package VPN 2.0
 # @since VPN 1.0
 # @author Drew Morris
-#
+# @mod by ZENO
 
 (
 
 VPN_IP=`curl ipv4.icanhazip.com>/dev/null 2>&1`
 
-VPN_USER="myuser"
-VPN_PASS="mypass"
+VPN_USER="test"
+VPN_PASS="test"
 
 VPN_LOCAL="192.168.0.150"
 VPN_REMOTE="192.168.0.151-200"
+PUB_IP="27.255.66.50"
 
-yum -y groupinstall "Development Tools"
-rpm -Uvh http://poptop.sourceforge.net/yum/stable/rhel6/pptp-release-current.noarch.rpm
-yum -y install policycoreutils policycoreutils
+rpm -i http://poptop.sourceforge.net/yum/stable/rhel6/pptp-release-current.noarch.rpm
 yum -y install ppp pptpd
-yum -y update
 
 echo "1" > /proc/sys/net/ipv4/ip_forward
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 
-sysctl -p /etc/sysctl.conf
 
 echo "localip $VPN_LOCAL" >> /etc/pptpd.conf # Local IP address of your VPN server
 echo "remoteip $VPN_REMOTE" >> /etc/pptpd.conf # Scope for your home network
@@ -39,13 +36,18 @@ echo "ms-dns 209.244.0.3" >> /etc/ppp/options.pptpd # Level3 Primary
 echo "ms-dns 208.67.222.222" >> /etc/ppp/options.pptpd # OpenDNS Primary
 
 echo "$VPN_USER pptpd $VPN_PASS *" >> /etc/ppp/chap-secrets
+sysctl -p
 
-service iptables start
-echo "iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/rc.local
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A INPUT -i venet0 -p tcp --dport 1723 -j ACCEPT
+iptables -A INPUT -i venet0 -p gre -j ACCEPT
+iptables -t nat -A POSTROUTING -j SNAT --to-source $VPN_LOCAL
+iptables -A FORWARD -i ppp0 -o venet0 -j ACCEPT
+iptables -A FORWARD -i venet0 -o ppp0 -j ACCEPT
 service iptables save
 service iptables restart
 
+iptables -t nat -I PREROUTING -d $PUB_IP -p 47 -j DNAT --to $VPN_LOCAL
+iptables -I FORWARD -d $VPN_LOCAL -p 47 -j ACCEPT
 service pptpd restart
 chkconfig pptpd on
 
